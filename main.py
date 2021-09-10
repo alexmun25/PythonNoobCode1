2dc0c5ac332265b50b9c02e1a4a4747dac4f71a6
AlexanderMuntyanu <muntyanu_81@list.ru>
2021-09-10 18:36:19 +0300

    To do list

--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 import numpy as np
 import pandas as pd
 see=pd.read_csv(r"C:\Users\AlexanderMuntyanu\Desktop\Data for Python\keywords.csv")
-print(see)
+see.head(n=10)
+tableEnd=see.tail(n=7)
+#This is a professional Python code. All rights reserved
+# ToDo:разобрать метод .shape ; установить пакет dtale; Найти датафрейм с цифрами; дальше читать книжку
+# ToDo: делать действия из книжки
+
